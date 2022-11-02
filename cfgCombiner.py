50673d0b62ef7ddbba94b67b5780d93f31bc5718
dexterdy <dexterdyalias@gmail.com>
2022-11-02 21:07:48 +0100

    made a script for combining configs and generated a large ref cfg

---
+++ cfgCombiner.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+fileOne = sys.argv[1]
+fileTwo = sys.argv[2]
+
+fileOneLines = []
+with open(fileOne) as reader:
+    lines = reader.readlines()
+    fileOneLines = [i.strip() for i in lines if i not in fileOneLines]
+
+fileTwoLines = []
+with open(fileTwo) as reader:
+    lines = reader.readlines()
+    fileTwoLines = [i.strip() for i in lines if i not in fileTwoLines]
+
+if len(fileOneLines) >= len(fileTwoLines):
+    large = fileOneLines
+    small = fileTwoLines
+else:
+    large = fileTwoLines
+    small = fileOneLines
+
+for i, line in enumerate(small):
+    putAfterThisIndex = 0
+    for x in range(i, 0, -1):
+        if small[x] in large:
+            putAfterThisIndex = large.index(small[x])
+            break
+
+    putBeforeThisIndex = len(large)
+    for x in range(i, len(small), 1):
+        if small[x] in large:
+            putBeforeThisIndex = large.index(small[x])
+            break
+
+    if putBeforeThisIndex != len(large):
+        large.insert(putBeforeThisIndex, line)
+    elif putAfterThisIndex != 0:
+        large.insert(putAfterThisIndex + 1, line)
+
+with open(sys.argv[3],'w') as writer:
+    for line in large:
+        writer.write(line + '\n')
